22ae75bb7933ca9f081ca44373c141c78db427cb
Tom DeCanio <decanio.tom@gmail.com>
2013-04-23 10:54:44 -0700

    Add broccoli output plugin

--- src/suricata.c
+++ src/suricata.c
@@ -100,6 +100,7 @@
 #include "alert-prelude.h"
 #include "alert-syslog.h"
 #include "alert-pcapinfo.h"
+#include "alert-broccoli.h"
 
 #include "log-droplog.h"
 #include "log-httplog.h"
@@ -1581,6 +1582,8 @@ int main(int argc, char **argv)
     TmModuleUnified2AlertRegister();
     /* pcap info log */
     TmModuleAlertPcapInfoRegister();
+    /* broccoli log */
+    TmModuleAlertBroccoliRegister();
     /* drop log */
     TmModuleLogDropLogRegister();
     /* http log */
